2eca7525d16d95d8b8856d8539e9c2127fb46dba
heldzr <contatohjr@gmail.com>
2025-04-15 19:44:03 -0300

    fix: ajustes em AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import RegisterPage from '../pages/Register/RegisterPage'
+import PersonalData from '../pages/PersonalData/PersonalData'
+import Profile from '../pages/Profile/Profile'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/register" />} />
       <Route path="/cadastro" element={<RegisterPage />} />
+      <Route path="/dadosPessoais" element={<PersonalData />} />
+      <Route path="/perfil" element={<Profile />} />
+
     </Routes>
   )
 }
